4bc89453e83dad89dbe0aaf18307da71cf1ebf5d
Kelly L <lock2846@gmail.com>
2018-04-03 10:16:54 -0600

    export tests pass

---
+++ migrations/20180403091547_books.js
@@ -0,0 +1,18 @@
+
+exports.up = (knex, Promise) => (
+  knex.schema.createTable('books', (t) => {
+    t.increments('id').primary()
+    t.varchar('title', 255).notNullable().defaultTo('')
+    t.varchar('author', 255).notNull().defaultTo('')
+    t.varchar('genre', 255).notNull().defaultTo('')
+    t.text('description').notNull().defaultTo('')
+    t.text('cover_url').notNull().defaultTo('')
+    // t.timestamp('created_at').notNullable().defaultTo(knex.fn.now())
+    t.dateTime('created_at').notNullable().defaultTo(knex.raw('now()'))
+    // t.timestamp('updated_at').notNullable().defaultTo(knex.fn.now())
+    t.dateTime('updated_at').notNullable().defaultTo(knex.raw('now()'))
+
+  })
+)
+
+exports.down = (knex, Promise) => knex.schema.dropTableIfExists('books')
