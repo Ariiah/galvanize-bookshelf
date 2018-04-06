77338188fa7048fb8d367c854a8d100aeba2c560
Kelly L <lock2846@gmail.com>
2018-04-06 11:43:05 -0600

    post request work with successfully hashing of password

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,26 @@
 'use strict';
 
-const express = require('express');
+const express = require('express')
+const knex = require('../knex')
+const humps = require('humps')
+const bcrypt = require('bcrypt')
+const router = express.Router()
 
-// eslint-disable-next-line new-cap
-const router = express.Router();
+router.post('/', (q, s, next) => {
+  const hashedPassword = bcrypt.hashSync(q.body.password, 5)
+  knex('users')
+  .insert({
+    'first_name': q.body.firstName,
+    'last_name': q.body.lastName,
+    'email': q.body.email,
+    'hashed_password': hashedPassword
+  })
+  .returning(['id', 'first_name', 'last_name', 'email'])
+  .then((newUser) => s.json(humps.camelizeKeys(newUser[0])))
+  .catch((err) => {
+    next(err)
+  })
+})
 
-// YOUR CODE HERE
 
-module.exports = router;
+module.exports = router
