6baf5243fd0f68834306226bebee74273c4acbf0
Kelly L <lock2846@gmail.com>
2018-04-09 11:58:28 -0600

    delete now works!

--- routes/token.js
+++ routes/token.js
@@ -20,8 +20,6 @@ router.get('/', (q, s, next) => {
 })
 
 router.post('/', (q, s, next) => {
-  // console.log('body', q.body);
-
   knex('users')
     .select('*')
     .where('email', q.body.email)
@@ -53,60 +51,15 @@ router.post('/', (q, s, next) => {
         })
       }
       else {
-        // console.log('bad email or password');
         next({ output: { statusCode: 400 }, message: 'Bad email or password'})
       }
-
     })
       .catch((err) => console.log('err', err))
-
 })
 
-// router.post('/', (q, s, next) => {
-//   knex('users')
-//   .select('*')
-//   // check if the email matches
-//   .where('email', q.body.email)
-//   .then((users) => {
-//     let foundUser = users[0]
-//     // need all info so it can pull all of it from record
-//     let {id, email, hashed_password, first_name, last_name} = foundUser
-//     // compares password and hashed password to be the same
-//     console.log('hashed_password:', hashed_password);
-//     console.log('KEY:', KEY);
-//     console.log('foundUser:', foundUser);
-//     bcrypt.compare(q.body.password, hashed_password, (err, result) => {
-//       // if(err) {
-//       //   s.status(401).send('not allowed')
-//       // }
-//       if (result) {
-//         const token = jwt.sign(foundUser, KEY)
-//         // s.cookie('token', token)
-//           s.cookie('token', token, { path: '/', httpOnly: true }).status(200).json(foundUser)
-//         // template literal to match what the test wants exactly
-//         // s.setHeader('set-cookie', `token=${token}; Path=\/; parseInt(HttpOnly)`)
-//       }
-//       // return s.status(200).send('worked!')
-//     })
-//         let info = {id, email, first_name, last_name}
-//         // create new obj so it can push the info it needs
-//         // if (!q.cookies.token){
-//         //   s.send(false)
-//         // }
-//         jwt.verify(q.cookies.token, KEY, (err, s) => {
-//           if (err){
-//             next(err)
-//           } else {
-//             next(foundUser)
-//           }
-//         })
-//         s.status(200).json(humps.camelizeKeys(info))
-//     })
-// })
+router.delete('/', (q, s, next) => {
+  s.setHeader('set-cookie', `token=; Path=\/; +HttpOnly`)
+  s.send(true)
+})
 
-// router.delete('/', (q, s, next) => {
-//   knex('user')
-//   .del()
-// })
-
-module.exports = router;
+module.exports = router
