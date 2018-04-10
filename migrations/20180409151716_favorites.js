9bfa619846b584818021c0cb9db35e898886adf5
Kelly L <lock2846@gmail.com>
2018-04-10 09:59:07 -0600

    searching for foreign keys works

--- migrations/20180409151716_favorites.js
+++ migrations/20180409151716_favorites.js
@@ -1,11 +1,14 @@
 
-exports.up = function(knex, Promise) {
+exports.up = (knex, Promise) => (
   knex.schema.createTable('favorites', (t) => {
-    t.increments('id').primary()
-    t.integer()
+    t.increments()
+    t.integer('user_id').notNullable()
+    t.foreign('user_id').references('id').inTable('users').onDelete('CASCADE')
+    t.integer('book_id').notNullable()
+    t.foreign('book_id').references('id').inTable('books').onDelete('CASCADE')
+    t.dateTime('created_at').notNull().defaultTo(knex.raw('now()'))
+    t.dateTime('updated_at').notNull().defaultTo(knex.raw('now()'))
   })
-};
+)
 
-exports.down = function(knex, Promise) {
-
-};
+exports.down = (knex, Promise) => knex.schema.dropTableIfExists('favorites')
