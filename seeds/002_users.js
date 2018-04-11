936d944a17663b5d50d278483ac6f0d5ccd1b04d
Kelly L <lock2846@gmail.com>
2018-04-11 15:54:27 -0600

    added promise end

--- seeds/002_users.js
+++ seeds/002_users.js
@@ -4,8 +4,8 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        // Inserts seed entries
-        knex('users').insert([{
+        knex('users').insert(
+          {
           id: 1,
           first_name: 'Joanne',
           last_name: 'Rowling',
@@ -13,7 +13,7 @@ exports.seed = function(knex, Promise) {
           hashed_password: '$2a$12$C9AYYmcLVGYlGoO4vSZTPud9ArJwbGRsJ6TUsNULzR48z8fOnTXbS',  // youreawizard
           created_at: new Date('2016-06-29 14:26:16 UTC'),
           updated_at: new Date('2016-06-29 14:26:16 UTC')
-      }])
+      })
       .then(() => {
         return knex.raw(`SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));`)
       })
