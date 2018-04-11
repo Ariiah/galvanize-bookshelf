58f5aa11c6b0a07b0698a469e043166011562f4d
Kelly L <lock2846@gmail.com>
2018-04-11 15:51:28 -0600

    edited foreign keys to one line

--- migrations/20180409151716_favorites.js
+++ migrations/20180409151716_favorites.js
@@ -2,10 +2,8 @@
 exports.up = (knex, Promise) => (
   knex.schema.createTable('favorites', (t) => {
     t.increments()
-    t.integer('user_id').notNullable()
-    t.foreign('user_id').references('id').inTable('users').onDelete('CASCADE')
-    t.integer('book_id').notNullable()
-    t.foreign('book_id').references('id').inTable('books').onDelete('CASCADE')
+    t.integer('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE')
+    t.integer('book_id').notNullable().references('id').inTable('books').onDelete('CASCADE')
     t.dateTime('created_at').notNull().defaultTo(knex.raw('now()'))
     t.dateTime('updated_at').notNull().defaultTo(knex.raw('now()'))
   })
