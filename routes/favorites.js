ebf049f5e29f3398ba79499c00cbd8efb9efb677
Kelly L <lock2846@gmail.com>
2018-04-11 15:53:43 -0600

    added middleware to check for authorized user

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,9 +1,77 @@
 'use strict';
 
 const express = require('express')
-
 const router = express.Router()
+const humps = require('humps')
+const knex = require('../knex')
+const jwt = require('jsonwebtoken')
 
-// YOUR CODE HERE
+const authenticated = (q, s, next) => {
+  // console.log(q);
+  if (q.cookies.token) {
+    jwt.verify(q.cookies.token, process.env.JWT_KEY, (err, payload) => {
+      if (err) next (err)
+      // console.log('payload?', payload);
+      q.token = payload
+    })
+  } else {
+    next({ output: { statusCode: 401 }, message: 'Unauthorized'})
+  }
+  next()
+}
+
+router.get('/', authenticated, (q, s, next) => {
+  knex('favorites')
+    // calls which table it needs, currentTable.key from other table, originalTable.key
+    .join('books', 'favorites.book_id', 'books.id')
+    .select('*')
+    .then((allFavs) =>  {
+        const camelized = allFavs.map((entry) => humps.camelizeKeys(entry))
+        s.json(camelized)
+    })
+    .catch((err) => console.log('err: ', err))
+})
+
+router.get('/check', authenticated, (q, s, next) => {
+  knex('favorites')
+  .where('book_id', q.query.bookId)
+  .join('books', 'favorites.book_id', 'books.id')
+  .select('*')
+  .returning('*')
+  .then((result) => {
+    if (result.length === 0) {
+      s.json(false)
+    }
+    else if (parseInt(q.query.bookId) === result[0].book_id){
+      s.json(true)
+    }
+  })
+})
+
+router.post('/', authenticated, (q, s, next) => {
+  const token = jwt.decode(q.cookies.token)
+  knex('favorites')
+    .insert({
+      'book_id': q.body.bookId,
+      'user_id': token.id
+    })
+    .returning('*')
+    .then((newFav) => s.json(humps.camelizeKeys(newFav[0])))
+})
+// BAD BAD BAD BAD BAD TEST CASE - MATTHEW B.
+router.delete('/', authenticated, (q, s, next) => {
+  const { bookId } = q.body
+  knex('favorites')
+    .where('book_id', bookId)
+    .del()
+    .returning('*')
+    // takes away the brackets
+    .then((favDeleted) => {
+      let [oldObj] = favDeleted
+      delete oldObj.id
+      s.json(humps.camelizeKeys(oldObj))
+
+    })
+})
 
 module.exports = router
